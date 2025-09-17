b864ec9c25bb509aab70083dc8311a02daecd38e
KimNgan <nhoochphan2212@gmail.com>
2025-09-17 13:53:20 +0700

    Update Program.cs

--- Lab1.2/Program.cs
+++ Lab1.2/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Lab1._2
+namespace Lab1._2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // Create instances of each figure
+            Figure triangle = new Triangle(3, 4);      // base = 3, height = 4
+            Figure circle = new Circle(5);             // radius = 5
+            Figure rectangle = new Rectangle(6, 2);    // width = 6, height = 2
+
+            // Display the area of each figure
+            Console.WriteLine($"Triangle area: {triangle.Area()}");
+            Console.WriteLine($"Circle area: {circle.Area()}");
+            Console.WriteLine($"Rectangle area: {rectangle.Area()}");
         }
     }
 }
